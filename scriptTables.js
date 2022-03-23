1c2bcd92b0826b60b99d253ceb369f1487df7951
Emmanuel Baricevic <72578567+Emmanuel-B-99@users.noreply.github.com>
2022-03-23 16:42:39 -0400

    Add files via upload

---
+++ scriptTables.js
@@ -0,0 +1,28 @@
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
+// LOLOLOLOLOL
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
